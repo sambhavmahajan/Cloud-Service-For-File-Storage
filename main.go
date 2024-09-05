e15a33e5fe94e0f7463979c219bc6103567f9903
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-09-05 19:19:22 +0530

    User Registration

--- main.go
+++ main.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"sync"
 )
 
-func main(){
-	r := gin.Default();
-	r.GET("/", func(c *gin.Context){
+var (
+	userData = make(map[string]string)
+
+	mu sync.RWMutex
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
 		c.File("./static/index.html")
 	})
-	
+	r.POST("/register", func(c *gin.Context) {
+		uname := c.PostForm("username")
+		pass := c.PostForm("password")
+		mu.RLock()
+		_, exists := userData[uname]
+		mu.RUnlock()
+		if exists {
+			c.String(http.StatusConflict, "Username already exists!")
+			return
+		}
+		mu.Lock()
+		userData[uname] = pass
+		mu.Unlock()
+		c.String(http.StatusOK, "User Created!")
+	})
 	r.Run()
 }
