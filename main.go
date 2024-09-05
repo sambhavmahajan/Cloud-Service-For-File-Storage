973d3ea0c008ba643e548376f23a6d3657787fe2
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-09-05 18:18:18 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	r := gin.Default();
+	r.GET("/", func(c *gin.Context){
+		c.File("./static/index.html")
+	})
+	
+	r.Run()
+}
