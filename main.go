b8660442960edacc76e6e36ae7304c9f6da32e43
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-09-05 22:45:28 +0530

    Keep track of uploaded files in Profile.

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	userData = make(map[string]string)
-
+	usernameToLinks = make(map[string][]string)
 	mu sync.RWMutex
 )
 
@@ -25,11 +25,11 @@ func makeNewUser(uname string, pass string) {
 	mu.Unlock()
 }
 
-func isValidUser(uname string, pass string) bool{
+func isValidUser(uname string, pass string) bool {
 	mu.RLock()
 	val, isValid := userData[uname]
 	mu.RUnlock()
-	if !isValid{
+	if !isValid {
 		return false
 	}
 	return val == pass
@@ -56,11 +56,11 @@ func main() {
 		pass := c.PostForm("password")
 		if userExists(uname) {
 			mu.RLock()
-			if userData[uname] == pass{
+			if userData[uname] == pass {
 				c.SetCookie("username", uname, 3600, "/", "localhost", false, true)
 				c.SetCookie("password", pass, 3600, "/", "localhost", false, true)
 				c.Redirect(http.StatusPermanentRedirect, "/user/"+uname)
-			}else {
+			} else {
 				c.String(http.StatusConflict, "Bad Login.")
 			}
 			mu.RUnlock()
@@ -69,14 +69,14 @@ func main() {
 		}
 	})
 
-	r.GET("/user/:username", func(c *gin.Context){
+	r.GET("/user/:username", func(c *gin.Context) {
 		uname := c.Param("username")
 		cache_name, err := c.Cookie("username")
-		if err != nil{
+		if err != nil {
 			c.String(http.StatusUnauthorized, "Can't view without login")
 			return
 		}
-		if uname != cache_name{
+		if uname != cache_name {
 			c.String(http.StatusUnauthorized, "Can't view without login")
 			return
 		}
@@ -88,43 +88,59 @@ func main() {
 			c.String(http.StatusUnauthorized, "Can't view without login")
 			return
 		}
-		c.String(200, "Login as " + uname)
+		slice, exists := usernameToLinks[uname]
+		res := "Login as "+uname + "\n\n" + "Files:\n"
+		if exists{
+			for _, x := range(slice) {
+				res = res + "\t" + x + "\n"
+			}
+		}
+		c.String(200, res)
+		
 		mu.RUnlock()
 	})
 
-	r.POST("/upload", func(c *gin.Context){
+	r.POST("/upload", func(c *gin.Context) {
 		uname, err1 := c.Cookie("username")
 		pass, err2 := c.Cookie("password")
-		if err1 != nil || err2 != nil || !isValidUser(uname, pass){
+		if err1 != nil || err2 != nil || !isValidUser(uname, pass) {
 			c.String(http.StatusUnauthorized, "Bad Credentials")
 			return
 		}
 		file, err := c.FormFile("file")
-		if err != nil{
+		if err != nil {
 			c.String(http.StatusBadRequest, "File could not be opened.")
 			return
 		}
 		c.SaveUploadedFile(file, "uploads/"+uname+"/"+file.Filename)
-		c.String(http.StatusOK, "File Successfuly Uploaded to: " + "uploads/"+uname+"/"+file.Filename)
+		path := "uploads/"+uname+"/"+file.Filename
+		c.String(http.StatusOK, "File Successfuly Uploaded to: "+path)
+		_, exists := usernameToLinks[uname]
+		if !exists{
+			usernameToLinks[uname] = make([]string, 1, 1)
+			usernameToLinks[uname][0] = path
+		}else{
+			usernameToLinks[uname] = append(usernameToLinks[uname], path)
+		}
 	})
-	r.GET("/uploads/:username/:filename", func(c *gin.Context){
+	r.GET("/uploads/:username/:filename", func(c *gin.Context) {
 		uname, err1 := c.Cookie("username")
 		pass, err2 := c.Cookie("password")
-		if err1 != nil || err2 != nil{
+		if err1 != nil || err2 != nil {
 			c.String(http.StatusUnauthorized, "Please Login.")
 			return
 		}
 		paramUserName := c.Param("username")
-		if uname != paramUserName{
+		if uname != paramUserName {
 			c.String(http.StatusUnauthorized, "Bad Login.")
 			return
 		}
-		if !isValidUser(uname, pass){
+		if !isValidUser(uname, pass) {
 			c.String(http.StatusUnauthorized, "Bad Login.")
 			return
 		}
 		paramFileName := c.Param("filename")
-		c.File("./uploads/"+uname+"/"+paramFileName)
+		c.File("./uploads/" + uname + "/" + paramFileName)
 	})
 	r.Run()
 }
